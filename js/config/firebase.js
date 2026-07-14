1073bbe24ba28be580701672a1335677a0057d59
benaiondelivery-png <benaiondelivery@gmail.com>
2026-07-14 01:44:11 -0300

    Create firebase.js

---
+++ js/config/firebase.js
@@ -0,0 +1,21 @@
+// ========================================
+// BENAION DELIVERY - FIREBASE CONFIG
+// ========================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCl-U9X9qxohjDpgr8y2pdkS3j-qNm19pk",
+  authDomain: "benaion-delivery.firebaseapp.com",
+  projectId: "benaion-delivery",
+  storageBucket: "benaion-delivery.firebasestorage.app",
+  messagingSenderId: "309927409217",
+  appId: "1:309927409217:web:7a105cb5237b2294b1b8c0",
+  measurementId: "G-TK1KNW14WH"
+};
+
+export const COLLECTIONS = {
+  USERS: 'users',
+  PEDIDOS: 'pedidos',
+  PRODUTOS: 'produtos',
+  AVALIACOES: 'avaliacoes',
+  CONFIG: 'config'
+};
